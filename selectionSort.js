c0fb6611dc25e1b8acb43701baa3fed7d67b5cfb
hritick1 <80475837+hritick1@users.noreply.github.com>
2023-01-25 22:13:04 +0530

    Added selection sort algorithms

--- selectionSort.js
+++ selectionSort.js
@@ -45,9 +45,49 @@ table.appendChild(tbody);
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    for(let i=0;i<num.length;i++){
+        let last=num.length-i-1;
+        let maxIndex=0;
+        await sleep(500)
+        
+   //finding max index
+   let max=0;
+   for(let j=0;j<last+1;j++){
+    await sleep(500)
+    document.getElementById("data" + [j]).classList.add('exchange');
+    if(parseInt(document.getElementById("data" + [j]).innerHTML)>max) {
+        max=parseInt(document.getElementById("data" + [j]).innerHTML);
+        await sleep(500)
+        // console.log(max);
+        if(j>0){
+            
+        document.getElementById("data" + [maxIndex]).classList.remove('exchange');
+        }
+        maxIndex=j;
+        await sleep(350)
+       
+        
+    }
+    document.getElementById("data" + [maxIndex]).classList.add('exchange');
+    await sleep(300)
+    document.getElementById("data" + [j]).classList.remove('exchange');
+   }
+   document.getElementById("data" + [last]).classList.add('exchange');
+   await sleep(500)
+   let temp = document.getElementById("data" + [maxIndex]).innerHTML;
+   console.log(last);
+   document.getElementById("data" + [maxIndex]).innerHTML = document.getElementById("data" + [last]).innerHTML;
+   await sleep(500)
+   document.getElementById("data" + [last]).innerHTML = temp;
+  
+   document.getElementById("data" + [last]).classList.remove('exchange');
+   document.getElementById("data" + [last]).classList.add('insert');
 
 
+    }
+   
 
-    
-
+}function cancel(){
+    window.location.reload();
+   
 }
